7d42424ddc32da8d2e1d45060bc62264b2ff5ca6
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-23 19:26:36 -0500

    feat: run test cases

--- src/GoldMeridian.PaintLabel.CLI/Program.cs
+++ src/GoldMeridian.PaintLabel.CLI/Program.cs
@@ -1,6 +1,20 @@
-﻿namespace GoldMeridian.PaintLabel.CLI;
+﻿using System;
+using System.IO;
+using GoldMeridian.PaintLabel.IO;
+
+namespace GoldMeridian.PaintLabel.CLI;
 
 internal static class Program
 {
-    public static void Main(string[] args) { }
+    public static void Main(string[] args)
+    {
+        var binaries = Directory.GetFiles(args[0], "*.fxc");
+        foreach (var path in binaries)
+        {
+            using var fs = File.OpenRead(path);
+            using var r = new BinaryReader(fs);
+            var effect = new EffectReader(r).ReadEffect();
+            Console.WriteLine(effect.HasErrors);
+        }
+    }
 }
