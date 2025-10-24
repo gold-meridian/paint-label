eb9b4e2405a0ada0f3774a6f8c96257f38cd9762
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-24 00:37:13 -0500

    feat: most shader reading support aside from parameter reading

--- src/GoldMeridian.PaintLabel/Shader/Shader.cs
+++ src/GoldMeridian.PaintLabel/Shader/Shader.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace GoldMeridian.PaintLabel.Shader;
@@ -29,7 +30,7 @@ public sealed class Shader : BaseShader<ShaderOpcode>
             case HEADER_CTAB:
                 ReadConstantTable(reader, length - 4);
                 return true;
-            
+
             case header_pres:
                 Preshader = Preshader.ReadPreshader(reader);
                 return true;
@@ -37,12 +38,58 @@ public sealed class Shader : BaseShader<ShaderOpcode>
 
         return false;
     }
-    
+
     protected override OpcodeData<ShaderOpcode> MakeRegularToken(BinaryReader reader, uint tokenType, uint length)
     {
-        throw new System.NotImplementedException();
+        var opcode = new OpcodeData<ShaderOpcode>
+        {
+            Type = (ShaderOpcode)tokenType,
+            Length = length,
+        };
+
+        if (length <= 0)
+        {
+            return opcode;
+        }
+
+        if (opcode.Type is ShaderOpcode.Call or ShaderOpcode.CallNz)
+        {
+            throw new NotImplementedException($"Unhandled opcode: \"{opcode.Type}\" (call or callnz)");
+        }
+        else if (opcode.Type == ShaderOpcode.Def)
+        {
+            opcode.Destination = DestinationParameter.Read(reader, Version);
+            opcode.Constants = [reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle()];
+        }
+        else if (opcode.Type == ShaderOpcode.Dcl)
+        {
+            opcode.Extra = reader.ReadUInt32();
+            opcode.Destination = DestinationParameter.Read(reader, Version);
+        }
+        else
+        {
+            var i = 0;
+
+            if (opcode.Length > i && !OpcodeTypeInfo.Opcodes[opcode.Type].NoDest)
+            {
+                opcode.Destination = DestinationParameter.Read(reader, Version);
+                i++;
+            }
+
+            if (opcode.Length > i)
+            {
+                var start = i;
+                opcode.Sources = new SourceParameter[opcode.Length - i];
+                for (; i < opcode.Length; i++)
+                {
+                    opcode.Sources[i - start] = SourceParameter.Read(reader, Version);
+                }
+            }
+        }
+
+        return opcode;
     }
-    
+
     protected override OpcodeData<ShaderOpcode> MakeComment(uint tokenType, uint length, byte[] readBytes)
     {
         return new OpcodeData<ShaderOpcode>
